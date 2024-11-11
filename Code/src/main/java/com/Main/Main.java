b2261b9550a9d03e1fc6ea5a84cd0bbf5cb54692
Franco Ignacio Rivas Vidal <francorivasvidal01@gmail.com>
2024-11-10 23:58:30 -0300

    Se agrego la clase Main.

---
+++ Code/src/main/java/com/Main/Main.java
@@ -0,0 +1,14 @@
+package com.Main;
+import com.Dom.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run(){
+        Dado dado = new Dado();
+        dado.jugarALosDados();
+    }
+}
